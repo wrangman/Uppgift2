611a6ff40231978c093459210e3cfe136a2f8220
wrangman <wrangman@gmail.com>
2026-08-31 09:20:13 +0200

    ficf

--- analyze_sales.py
+++ analyze_sales.py
@@ -1,49 +1,52 @@
-from collections import Counter
 import csv
-import locale
 import subprocess
+import locale
+from collections import Counter
+
 
 
 def load_sales(filename):
-   products = {}  #ordbok dictionary
-   list_products = []
+   products = {}
+   all_products = []
    
    with open(filename, 'r') as file:
       reader = csv.DictReader(file)
-      
       for row in reader:
          product = row['Product']
          sales = float(row['Sales'])
          
-         list_products.append(product)
+         all_products.append(product)
          
          if product in products:
                products[product] += sales
          else:
                products[product] = sales
+
                
-   return products
-                
-def analyze_sales_data(products):
-      
-   #TODO: Hitta den mest sålda produkten (TIPS! Använd Counter från collections)
-   most_common_product = Counter(products)
-   most_common = most_common_product.most_common(1)[0]
+   return products, all_products
 
-   #TODO: Hitta den mest lukrativa produkten med max: max(products, key=products.get)
-   most_lucrative_product = 0
+def analyze_sales_data(products, all_products):
+
+   # Hitta den mest sålda produkten
+   product_count = Counter(all_products)
+   most_common_product = product_count.most_common(1)[0]
+
+   #TODO: Hitta den mest lukerativa produkten och använd max(products, key=products.get) (PS: products.get är en liten funktion som hämtar alla produkter i dictionary)
    
-   print(f"Mest sålda produkt: {most_common[0]}, Antal: {most_common[1]}")
-   print(f"Mest lukrativa produkt: \"{most_lucrative_product}\" med försäljning på {locale.currency(0,grouping=True)}") #TODO: BONUS: kan du skapa en funktion som skriver ut rätt formaterad valuta istället för detta?
+   most_lucrative_product = 0
+   product_value = 0 # FIXME: använd  products[most_lucrative_product]
+   
+   print(f"Mest lukrativa produkt: \"{most_lucrative_product}\" med försäljning på {locale.currency(0,grouping=True)}")  #FIXME: Lägg in product_value här
+   
+   #TODO: skapa gärna en funktion för format_currency här.
+   print(f"Mest sålda produkt: {most_common_product[0]}, Antal: {most_common_product[1]}")
 
+
+# Sätt språkinställning till svenska (Sverige)
+locale.setlocale(locale.LC_ALL, 'sv_SE.UTF-8')
 
 subprocess.run('cls', shell=True)
 
-# Sätt språkinställning till svenska (Sverige) används för att skriva ut formaterad valuta
-locale.setlocale(locale.LC_ALL, 'sv_SE.UTF-8')  
 
-products = load_sales('sales_data.csv')
-
-print(products)
-analyze_sales_data(products)
-
+products, all_products = load_sales('sales_data.csv')
+analyze_sales_data(products, all_products)
